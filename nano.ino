4732f3b428319522e2eb6016912f4c7251de5626
Harry Ross <hazross@hotmail.com>
2020-05-27 16:38:40 +1000

    added arduino file

---
+++ nano.ino
@@ -0,0 +1,70 @@
+/*
+ * 
+ * All the resources for this project: https://randomnerdtutorials.com/
+ * Modified by Rui Santos
+ * 
+ * Created by FILIPEFLOP
+ * 
+ */
+ 
+#include <SPI.h>
+#include <MFRC522.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial hc06(2,3);
+
+#define LED_PIN 7 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   
+  SPI.begin();      
+  mfrc522.PCD_Init();   
+  Serial.println("Please hold card to reader");
+
+  hc06.begin(9600);
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UjID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  
+  hc06.print(content.substring(1) + ';');
+  delay(1000);                  
+  delay(500);
+ 
+  if (hc06.available()){
+    Serial.write(hc06.read());
+  }
+  
+  //Write from Serial Monitor to HC06
+  if (Serial.available()){
+    hc06.write(Serial.read());
+  }
+} 
